e62972d12c26982b0cec6d74f2c2a30b4a4bd460
heloowird <heloowird@gmail.com>
2016-09-16 23:22:06 +0800

    add neural node

---
+++ neural_node.py
@@ -0,0 +1,24 @@
+#coding=utf-8
+
+import active_func as af
+
+'''
+neural node
+'''
+class Node:
+	def __init__(self, active_func_type):
+		self.func_type       = active_func_type
+		self.forward_input   = 0.0
+		self.forward_output  = 0.0
+		self.backward_input	 = 0.0
+		self.backward_output = 0.0
+	
+	# forward
+	def forward(self, x):
+		self.forward_input = x
+		self.forward_output = af.forward(self.func_type, self.forward_input) 
+	
+	# backward
+	def backward(self, x):
+		self.backward_input = x
+		self.backward_output = af.backward(self.func_type, self.forward_output, self.backward_input)
