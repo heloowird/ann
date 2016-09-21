ed0017c5f95c7689bc12c5673593c64c790fdff6
Jianqi Zhu <heloowird@gmail.com>
2016-09-22 00:05:22 +0800

    Update active_func.py

--- active_func.py
+++ active_func.py
@@ -10,26 +10,26 @@ def forward(func_type, x):
 	if func_type == 0:
 		return x
 	elif func_type == 1:
-		return log_sigmoid(x)
+		return sigmoid(x)
 	else:
-		return tan_sigmoid(x)
+		return tanh(x)
 
 def backward(func_type, y, x):
 	if func_type == 0:
 		return x
 	elif func_type == 1:
-		return dr_log_sigmoid(y, x)
+		return dr_sigmoid(y, x)
 	else:
-		return dr_tan_sigmoid(y, x)
+		return dr_tanh(y, x)
 
-def log_sigmoid(x):
+def sigmoid(x):
 	return (1 / (1 + math.exp(-x)))
 
-def dr_log_sigmoid(y, x):
+def dr_sigmoid(y, x):
 	return (y * (1 - y) * x)
 
-def tan_sigmoid(x):
+def tanh(x):
 	return ((math.exp(x) - math.exp(-x)) / (math.exp(x) + math.exp(-x)))
 
-def dr_tan_sigmoid(y, x):
+def dr_tanh(y, x):
 	return ((1 - math.pow(y, 2)) * x)
