c4d3b31cfb77bdf5f5de08ddd210dc9dc8db933e
Jianqi Zhu <heloowird@gmail.com>
2016-09-22 00:10:34 +0800

    Update neural_node.py

--- neural_node.py
+++ neural_node.py
@@ -1,13 +1,13 @@
 #coding=utf-8
 
-import active_func as af
+import activation_func as af
 
 '''
 neural node
 '''
 class Node:
-	def __init__(self, active_func_type):
-		self.active_func_type = active_func_type
+	def __init__(self, activation_func_type):
+		self.af_type = activation_func_type
 		self.forward_input = 0.0
 		self.forward_output = 0.0
 		self.backward_input = 0.0
@@ -16,9 +16,9 @@ class Node:
 	# forward
 	def forward(self, x):
 		self.forward_input = x
-		self.forward_output = af.forward(self.active_func_type, self.forward_input) 
+		self.forward_output = af.forward(self.af_type, self.forward_input) 
 	
 	# backward
 	def backward(self, x):
 		self.backward_input = x
-		self.backward_output = af.backward(self.active_func_type, self.forward_output, self.backward_input)
+		self.backward_output = af.backward(self.af_type, self.forward_output, self.backward_input)
