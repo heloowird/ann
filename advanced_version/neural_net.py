bd77bbc3e35d50fb84ad6d1849c0b6d5dc7f1beb
heloowird <heloowird@gmail.com>
2016-11-02 23:52:07 +0800

    add bias

--- advanced_version/neural_net.py
+++ advanced_version/neural_net.py
@@ -4,20 +4,23 @@ import sys
 from neural_input_layer import InputLayer
 from neural_sigmoid_layer import SigmoidLayer
 import numpy as np
+import time
 
 '''
-The simple neural net
+The simple neural net only contains:
+	one input layer
+	one hidden layer
+	one output layer
 '''
 class Net:
 	# initalize neural net
-	def __init__(self, batch_size, input_dim, hidden_dim, output_dim):
+	def __init__(self, batch_size, input_dim, hidden_dim, output_dim, input_hidden_w=None, input_hidden_b=None, hidden_output_w=None, hidden_output_b=None):
 		self.input_dim = input_dim
 		self.hidden_dim = hidden_dim
 		self.output_dim = output_dim
 		
 		# initailize weights
-		self.input_hidden_wgt = np.random.random((self.input_dim, self.hidden_dim)) * 0.001
-		self.hidden_output_wgt = np.random.random((self.hidden_dim, self.output_dim)) * 0.001
+		self.init_weight(input_hidden_w, input_hidden_b, hidden_output_w, hidden_output_b)
 
 		# initialize layers
 		self.input_layer = InputLayer()
@@ -27,6 +30,28 @@ class Net:
 		# initialize data of layers
 		self.reset(batch_size)
 	
+	def init_weight(self, input_hidden_w, input_hidden_b, hidden_output_w, hidden_output_b):
+		# initailize weights
+		if input_hidden_w is None:
+			self.input_hidden_w = np.random.random((self.input_dim, self.hidden_dim)) * 0.01
+		else:
+			self.input_hidden_w = input_hidden_w
+
+		if input_hidden_b is None:
+			self.input_hidden_b = np.zeros((1, self.hidden_dim))
+		else:
+			self.input_hidden_b = input_hidden_b
+
+		if hidden_output_w is None:
+			self.hidden_output_w = np.random.random((self.hidden_dim, self.output_dim)) * 0.01
+		else:
+			self.hidden_output_w = hidden_output_w
+
+		if hidden_output_b is None:
+			self.hidden_output_b = np.zeros((1, self.output_dim))
+		else:
+			self.hidden_output_b = hidden_output_b
+
 	def reset(self, batch_size):
 		self.batch_size = batch_size
 
@@ -36,32 +61,30 @@ class Net:
 			print >>sys.stderr, "bad batch: input_num=%d, batch_size=%d" % (data.shape[0], self.batch_size)
 			exit()
 		self.input_layer.forward(data)
-		self.hidden_layer.forward(np.dot(self.input_layer.forward_output, self.input_hidden_wgt))
-		self.output_layer.forward(np.dot(self.hidden_layer.forward_output, self.hidden_output_wgt))
+		self.hidden_layer.forward(np.dot(self.input_layer.forward_output, self.input_hidden_w) + self.input_hidden_b)
+		self.output_layer.forward(np.dot(self.hidden_layer.forward_output, self.hidden_output_w) + self.hidden_output_b)
 
 	# backward
 	def backward(self, label):
 		if label.shape[0] != self.batch_size:
 			print >>sys.stderr, "bad batch: input_num=%d, batch_size=%d" % (label.shape[0], self.batch_size)
 			exit()
-		label.shape = (self.batch_size, 1)
-		result = np.zeros((self.batch_size, self.output_dim))
-		for i in range(self.batch_size):
-			result[i][int(label[i][0])] = 1.0
-		self.output_layer.backward(self.output_layer.forward_output - result)
-		self.hidden_layer.backward(np.dot(self.output_layer.backward_output, np.transpose(self.hidden_output_wgt)))
+		self.output_layer.backward(self.output_layer.forward_output - label)
+		self.hidden_layer.backward(np.dot(self.output_layer.backward_output, np.transpose(self.hidden_output_w)))
 
 	# update
 	def update(self, eta):
 		sum_deta_input_hidden = np.zeros((self.input_dim, self.hidden_dim))
 		for i in range(self.batch_size):
 			sum_deta_input_hidden += np.dot(np.transpose(self.input_layer.forward_output[i:i+1, :]), self.hidden_layer.backward_output[i:i+1, :])
-		self.input_hidden_wgt -= (eta / self.batch_size) * sum_deta_input_hidden
+		self.input_hidden_w -= (eta / self.batch_size) * sum_deta_input_hidden
+		self.input_hidden_b -= (eta / self.batch_size) * (np.cumsum(self.hidden_layer.backward_output)[-1])
 
 		sum_deta_hidden_output = np.zeros((self.hidden_dim, self.output_dim))
 		for i in range(self.batch_size):
 			sum_deta_hidden_output += np.dot(np.transpose(self.hidden_layer.forward_output[i:i+1, :]), self.output_layer.backward_output[i:i+1, :])
-		self.hidden_output_wgt -= (eta / self.batch_size) * sum_deta_hidden_output
+		self.hidden_output_w -= (eta / self.batch_size) * sum_deta_hidden_output
+		self.hidden_output_b -= (eta / self.batch_size) * (np.cumsum(self.output_layer.backward_output)[-1])
 
 	def train(self, data, label, eta):
 		if data.shape[0] != self.batch_size:
@@ -70,7 +93,7 @@ class Net:
 		self.backward(label)
 		self.update(eta)
 	
-	def test(self, data, label):
+	def get_accuracy(self, data, label):
 		if data.shape[0] < 1:
 			return 0.0
 
@@ -78,11 +101,9 @@ class Net:
 			self.reset(data.shape[0])
 
 		self.forward(data)
-
-		label.shape = (data.shape[0], 1) 
 		predict_label = np.argmax(self.output_layer.forward_output, axis=1)
-		predict_label.shape = (data.shape[0], 1)
-		right_cnt = np.sum((predict_label - label - 0.0) == 0.0)
+		true_label = np.argmax(label, axis=1)
+		right_cnt = np.sum((predict_label - true_label) == 0)
 		return 1.0 * right_cnt / data.shape[0]
 
 	def predict(self, data):
@@ -94,6 +115,5 @@ class Net:
 
 		self.forward(data)
 		predict_label = np.argmax(self.output_layer.forward_output, axis=1)
-		predict_label.shape = (data.shape[0], 1)
 		predict_label = predict_label.astype(int)
 		return predict_label
--- advanced_version/train_and_test.py
+++ advanced_version/train_and_test.py
@@ -1,57 +1,84 @@
 #coding=utf-8
 
+import os
 import numpy as np
 import pandas as pd
 import neural_net
-import random
 import time
 
-def train_ann_classifier(step_num, eta):
-	raw_train_data = pd.read_csv("./data/train.csv")
+def load_pre_train_weight(filename, shape):
+	if os.path.isfile(filename):
+		weight = np.fromfile(filename, dtype=np.float)
+		weight.shape = shape
+		return weight
+	else:
+		return None
 
-	feature_num = len(raw_train_data.columns) - 1
-	hidden_node_num = int(feature_num * 1.5)
+def load_one_hot_data(filename):
+	raw_train_data = pd.read_csv(filename)
+	train_data = raw_train_data.values[:]
+	return train_data
+
+def labels_to_one_hot(raw_labels):
+	labels = np.zeros((raw_labels.shape[0], 10))
+	labels[np.arange(raw_labels.shape[0]), raw_labels] = 1
+	labels = labels.astype(np.float32)
+	return labels
+
+def gen_batch_data(train_data, batch_size):
+	batch_index = np.random.randint(0, train_data.shape[0], batch_size)
+	batch_train_data = train_data[batch_index]
+	return (batch_train_data[:, 1:] / 255.0, labels_to_one_hot(batch_train_data[:, 0]))
+
+def train_ann_classifier(step_num, eta):
+	raw_train_data = load_one_hot_data("./data/train.csv")
+
+	feature_num = raw_train_data.shape[1] - 1
+	hidden_node_num = 13
 	label_num = 10
-	batch_size = 100
+	batch_size = 50
 
 	print "feature_num: %d" % feature_num
 	print "hidden_node_num: %d" % hidden_node_num
 	print "label_num: %d" % label_num
 	print "batch_size: %d" % batch_size
-	
+
+	# load pre-trained weight and bias
+	input_hidden_w = load_pre_train_weight("./model/input_hidden_w.bin", (feature_num, hidden_node_num))
+	input_hidden_b = load_pre_train_weight("./model/input_hidden_b.bin", (1, hidden_node_num))
+	hidden_output_w = load_pre_train_weight("./model/hidden_output_w.bin", (hidden_node_num, label_num))
+	hidden_output_b = load_pre_train_weight("./model/hidden_output_b.bin", (1, label_num))
 
 	# initialize simple ann
-	ann_net = neural_net.Net(batch_size, feature_num, hidden_node_num, label_num)
+	ann_net = neural_net.Net(batch_size, feature_num, hidden_node_num, label_num, input_hidden_w, input_hidden_b, hidden_output_w, hidden_output_b)
 
-	# normalization
-	raw_train_data = raw_train_data.values[:]
-	train_data = (raw_train_data - np.min(raw_train_data)) / (np.max(raw_train_data) * 1.0 - np.min(raw_train_data))
-	
 	# train
+	np.random.shuffle(raw_train_data)
+	train_len = int(raw_train_data.shape[0] * 0.8)
+	train_data, valid_data = raw_train_data[:train_len], raw_train_data[train_len:]
+	valid_features, valid_labels = valid_data[:, 1:] / 255.0, labels_to_one_hot(valid_data[:, 0])
+	
 	for i in xrange(step_num):
-		np.random.shuffle(train_data)
-		for j in xrange(train_data.shape[0]/batch_size):
-			train_batch = train_data[j*batch_size : (j+1)*batch_size]
-			start_t = time.time()
-			ann_net.train(train_batch[:, 1:], train_batch[:, 0], eta)
-			print "%d epoch and %d batch cost time:  %d ms" % (i, j, int((time.time() - start_t)*1000))
+		features_data, labels_data = gen_batch_data(train_data, batch_size)
+		#start_t = time.time()
+		ann_net.train(features_data, labels_data, eta)
+		#print "%d batch cost time:  %d ms" % (i, int((time.time() - start_t)*1000))
 
-		# print accuracy of every 100 iterion
-		if i % 100 == 0:
-			start = random.randint(0, raw_train_data.shape[0] - batch_size)
-			test_batch = raw_train_data[start : start+batch_size]
-			print "accuracy of %d batch: %f" % (i, ann_net.test(test_batch[:, 1:], test_batch[:, 0]))
+		# print accuracy of every one epoch
+		if (i+1) == 1 or (i+1) % 840 == 0 or (i+1) == step_num:
+			print "accuracy of %d batch (%d epoch): %f" % ((i+1), (i+1)/840, ann_net.get_accuracy(valid_features, valid_labels))
 
-	# save_model
-	ann_net.input_hidden_wgt.tofile("input_hidden_wgt.bin")
-	ann_net.hidden_output_wgt.tofile("hidden_output_wgt.bin")
+	ann_net.input_hidden_w.tofile("./model/input_hidden_w.bin")
+	ann_net.input_hidden_b.tofile("./model/input_hidden_b.bin")
+	ann_net.hidden_output_w.tofile("./model/hidden_output_w.bin")
+	ann_net.hidden_output_b.tofile("./model/hidden_output_b.bin")
 
 	# test
-	test_data = pd.read_csv("./data/test.csv")
-	predict_label = ann_net.predict(test_data[0:].values[:])
-	predict_label.shape = (predict_label.shape[0], 1)
-	# save prediction
-	np.savetxt("submission.csv", predict_label, fmt="%d")
+	test_data = load_one_hot_data("./data/test.csv")
+	predict_label = ann_net.predict(test_data / 255.0)
+	predict_label = [np.arange(1, 1+len(predict_label)), predict_label]
+	predict_label = np.transpose(predict_label)
+	np.savetxt('submission.csv', predict_label, fmt='%i,%i', header='ImageId,Label', comments='')
 
 if __name__ == "__main__":
-	train_ann_classifier(1000, 0.1)
+	train_ann_classifier(20*1000, 0.01)
